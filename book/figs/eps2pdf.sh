0b3ea479baf78ac22440ab82ab7c5bef06b31f7e
Chris Mayfield <mayfiecs@jmu.edu>
2019-08-06 11:27:58 -0400

    remove char arrays from strings

---
+++ book/figs/eps2pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# convert EPS image to PDF version 1.4
+
+if [ -z $1 ]
+then
+    echo "usage: $0 file.eps"
+    exit 1
+fi
+
+epstopdf $1
+PDF=${1%.eps}.pdf
+
+gs -dBATCH -dNOPAUSE -dQUIET -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile=TEMP.pdf $PDF
+mv TEMP.pdf $PDF
